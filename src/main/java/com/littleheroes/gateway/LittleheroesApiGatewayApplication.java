0470c82abb1cfc944cdc85f021c7c39e7aff9b60
Thabang Kwinana <bennythabang43@gmail.com>
2026-01-10 09:47:37 +0200

    Initial commit

---
+++ src/main/java/com/littleheroes/gateway/LittleheroesApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.littleheroes.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleheroesApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleheroesApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/littleheroes/gateway/LittleheroesApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littleheroes.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleheroesApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
